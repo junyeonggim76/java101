71a15a835cc0dbd22f748902ee944607905f6ad5
junyeonggim76 <rlawnsdud0401@naver.com>
2024-05-10 21:47:12 +0900

    Add files via upload

---
+++ day01/Quiz_java.java
@@ -0,0 +1,11 @@
+package day01;
+
+public class Quiz_java {
+    public static void main(String[] args) {
+        int A = 13;
+        System.out.println("1. 수강하면서 재밌었다.");
+        System.out.println("2. 다음 수업 날짜는");
+        System.out.println(A);
+    }
+
+}
---
+++ day01/variable.java
@@ -0,0 +1,29 @@
+package day01;
+
+public class variable {
+    public static void main(String[] args) {
+        //integer(정수)
+        //int a = 123;
+        //int b = 100;
+        // int c = 7;
+        //System.out.println(a+b);
+
+        //int height = 178;
+        //System.out.println("제가 좋아하는 유튜버는 핫소스이고 키는 ");
+        //System.out.println(height);
+        //int a = 2;
+        //int b = 60;
+        //int c = 86;
+        //System.out.println("지금 코스닥 주식은");
+        //System.out.println(a);
+        //System.out.println("지금 나스닥 주식은");
+        //System.out.println(b);
+        //System.out.println("지금 비트코인은");
+        //System.out.println(c);
+        int Quiz_java = 13;
+        System.out.println("1. 수강하면서 재밌었다.");
+        System.out.println("2. 다음 수업 날짜는");
+        System.out.println(Quiz_java);
+
+    }
+}
